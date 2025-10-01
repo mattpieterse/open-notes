11b4634ea1ddf3c492cbd0d96678c0d5b39d4988
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-10-01 16:22:57 +0200

    feat: create initial project scaffolding

---
+++ open-notes/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="h-screen w-screen font-sans overflow-x-hidden">
+            Operational
+        </div>
+    );
+}
+
+export default App
